82f641a50887f3e70f6ee99b7a1cca2c322bc369
KarolCifuentes <karolsofi16@gmail.com>
2023-06-21 23:26:09 -0500

    Primer commit react con vitejs

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Header = ({logo, menuItem, children}) =>{
+    return (
+        <header>
+            <h3>Logo</h3>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">My work</a>
+                <a href="#">Blog</a>
+                <a href="#">About me</a>
+
+                <button>
+                    
+                </button>
+            </nav>
+            <button>
+                
+            </button>
+
+        </header>
+    )
+}
+
+export default header;
